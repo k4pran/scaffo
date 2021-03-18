732427a7ea5ac9dec75116d2307e3db2d24acb1a
ryan <ryanmccauley211@gmail.com>
2021-03-18 19:20:53 +0000

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='agent-frame',
-    version='0.2.3',
+    version='0.2.4',
     packages=['agent_frame'],
     url='http://www.ryanjames.io',
     license='',
